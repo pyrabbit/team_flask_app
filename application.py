6dbf0af117cf1e8371b48e396c64ae452579bba5
Matt Orahood <morahood@gmail.com>
2018-08-27 18:23:06 -0400

    changed from team to world

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def welcome():
-    return "Hello Team! This is a Flask application for CMSC495-6381."
+    return "Hello World! This is a Flask application for CMSC495-6381."
 
 
 if __name__ == "__main__":
